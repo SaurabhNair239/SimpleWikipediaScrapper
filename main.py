a187951b5d6bd326e6108e6c86c972edf16dc227
SaurabhNair239 <58506467+SaurabhNair239@users.noreply.github.com>
2021-12-05 12:07:13 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import pandas as pd
+def Crawler(main_link,main_page):
+  if main_link and main_page:
+    i = 0
+    Page_count = []
+    internal_link_count = []
+    external_link_count = []
+    URLFragment_link_count = []
+    timestamps = []
+    internal_link = set()
+    internal_link.add(main_page)
+    while i <= 200:
+      if len(internal_link) > 0:
+
+          internal_counter = set()
+          external_counter = set()
+          URLFragment_counter = set()
+          all_pages = set()
+          inter = internal_link.pop()
+          html = requests.get(main_link+inter).text
+          soap_obj = BeautifulSoup(html,'html.parser')
+          for links in soap_obj.find_all("a"):
+              all_pages.add(links.get('href'))
+          #internal Link
+          for link in all_pages:
+              if link:
+                  if link.startswith('/wiki/'):
+                      if i == 0:
+                          internal_link.add(link)
+                      else:
+                          internal_counter.add(link)
+          if i == 0:
+              internal_link_count.append(len(internal_link))
+          else:
+              internal_link_count.append(len(internal_counter))
+
+          #External Link
+          for link in all_pages:
+              if link:
+                  if link.startswith('https'):
+                      external_counter.add(link)
+          external_link_count.append(len(external_counter))
+
+          # URLFragment Link
+          for link in all_pages:
+              if link:
+                  if link.startswith('#'):
+                      URLFragment_counter.add(link)
+          URLFragment_link_count.append(len(URLFragment_counter))
+
+          #timestamp
+          timestamp = soap_obj.find_all('script', type='application/ld+json')
+          for time in timestamp:
+            if len(time.text) > 0:
+              date_time = json.loads(time.text)['dateModified']
+            else:
+              date_time = 'None'
+          timestamps.append(date_time)
+          Page_count.append(i+1)
+          i+=1
+      else:
+          dict = {'Pagecount':Page_count,'INTcount':internal_link_count,'EXTcount':external_link_count,'URLfragments':URLFragment_link_count,'timestammp':timestamps}
+          return pd.DataFrame(dict)
+          break
+Data = Crawler('https://simple.wikipedia.org', '/wiki/Climate_change')
+print(Data)
